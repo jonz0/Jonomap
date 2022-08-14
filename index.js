0731d3db7fa3f410cfaedfe48ee0db50ec03d06d
jonz0 <ijonluu@gmail.com>
2022-08-13 20:24:17 -0700

    WIP: hiding API key using dotenv

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.get('/', (req, res) => res.json({ success: 'Hello World!'}));
+app.listen(port, () => console.log(`App listening on port $(port)`));
--- js/jonomap-script.js
+++ js/jonomap-script.js
@@ -1,18 +1,16 @@
-const mapsKey = config.MAPS_API_KEY;
+const maps_api = process.env.MAPS_API_KEY;
+const mapsUrl = 'https://maps.googleapis.com/maps/api/js?key=' + process.env.MAPS_API_KEY + '&callback=initMap&v=weekly';
 
-window.addEventListener('load', () => {
-    document.getElementById('request').src = `https://maps.googleapis.com/maps/api/js?key=' + mapsKey + '&callback=initMap&v=weekly`;
-});
 
+// Initialize and add the map
 function initMap() {
-    var location = {lat: -25.363, lng: 131.044};
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4, 
-        center: location
-    });
-
-    var marker =  new maps.google.Marker({
-        position: location,
-        map: map
-    });
+  const uluru = { lat: -25.344, lng: 131.031 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: uluru,
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
